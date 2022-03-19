e6c94f5c254a70feab6dfa948c65c42be36bc8a7
Ranjith Venkatesh <ranjith.venkatesh@mossandlichens.com>
2022-03-19 07:17:10 +0100

    Initial commit

---
+++ src/Moss and Lichens Labs/Pages/ScrumValuesChart/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿namespace Moss_and_Lichens_Labs.Pages.ScrumValuesChart.Hubs
+{
+    using Microsoft.AspNetCore.SignalR;
+    using System.Threading.Tasks;
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }    
+}
---
+++ src/Moss and Lichens Labs/Pages/ScrumValuesChart/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Moss_and_Lichens_Labs.Pages.ScrumValuesChart
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
